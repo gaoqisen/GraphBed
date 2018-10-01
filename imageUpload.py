0fef45373dde6d5beb7a4609c3486b6117fdd872
gaoqisen <1073825890@qq.com>
2018-10-01 22:08:49 +0800

    20181001220844.png add

--- imageUpload.py
+++ imageUpload.py
@@ -1,6 +1,14 @@
 #!/usr/bin/python
 import os
 import time
+def txt(text):
+	file = open('github_image_path.txt','w')
+	file.write(text)
+	file.close()
+	print('ok')
+	os.system('open github_image_path.txt')
+	
+
 fileName=time.strftime("%Y%m%d%H%M%S", time.localtime())+'.png'
 print(fileName)
 upload=os.system('screencapture -i '+fileName)
@@ -12,5 +20,5 @@ if(upload==0):
 	os.system('git push origin master')
 	str='https://gaoqisen.github.io/GraphBed/'+fileName
 	print(str)
-	os.system('notepad python.txt')
+	txt(str)
 	print('end')
