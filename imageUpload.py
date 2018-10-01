b10944456a795dc9c23f04d6dc728bf490f06eb5
gaoqisen <1073825890@qq.com>
2018-10-01 21:48:25 +0800

    20181001214822.png add

---
+++ imageUpload.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+import os
+import time
+fileName=time.strftime("%Y%m%d%H%M%S", time.localtime())+'.png'
+print(fileName)
+upload=os.system('screencapture -i '+fileName)
+print(upload)
+if(upload==0):
+	print('success')
+	os.system('git add .')
+	os.system('git commit -m "'+fileName+' add"')
+	os.system('git push origin master')
+	print('end')
