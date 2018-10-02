27b3339166eedf3591e4425098a42d0b042784f6
gaoqisen <1073825890@qq.com>
2018-10-02 13:06:41 +0800

    201810/20181002130636.png add

--- imageUpload.py
+++ imageUpload.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 import os
 import time
+
 def txt(text):
 	print(text)
 	file = open('github_image_path.txt','w')
@@ -10,13 +11,18 @@ def txt(text):
 	os.system('open github_image_path.txt')
 	os.system('say "ok"')
 
+# create file
 dir=time.strftime("%Y%m", time.localtime())
 fileName=dir+'/'+time.strftime("%Y%m%d%H%M%S", time.localtime())+'.png'
-if not os.path.exists(dir):
-        os.makedirs(dir)
-print(fileName)
-upload=os.system('screencapture -i '+fileName)
-print(upload)
+path='/Users/gaoqisen/Documents/blog/image/GraphBed'
+print(path+dir)
+if not os.path.exists(path+dir):
+        os.makedirs(path+dir)
+print(path+fileName)
+# screencapture
+upload=os.system('screencapture -i '+path+fileName)
+
+
 if(upload==0):
 	os.system('git add .')
 	os.system('git commit -m "'+fileName+' add"')
