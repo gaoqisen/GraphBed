b90aa0618252832455c7672428dcf3cd14772094
gaoqisen <1073825890@qq.com>
2018-10-02 15:15:09 +0800

    201810/20181002151506.png add

--- imageUpload.py
+++ imageUpload.py
@@ -3,29 +3,27 @@ import os
 import time
 
 def txt(text):
-	print(text)
-	file = open('github_image_path.txt','w')
-	file.write(text)
-	file.close()
-	print('ok')
-	os.system('open github_image_path.txt')
-	os.system('say "ok"')
+        file = open('github_image_path.txt','w')
+        file.write(text)
+        file.close()
+        os.system('open github_image_path.txt')
+        os.system('say "ok"')
 
 # create file
 dir=time.strftime("%Y%m", time.localtime())
 fileName=dir+'/'+time.strftime("%Y%m%d%H%M%S", time.localtime())+'.png'
-path='/Users/gaoqisen/Documents/blog/image/GraphBed'
-print(path+dir)
+path='/Users/gaoqisen/Documents/blog/image/GraphBed/'
 if not os.path.exists(path+dir):
         os.makedirs(path+dir)
-print(path+fileName)
 # screencapture
 upload=os.system('screencapture -i '+path+fileName)
 
 
 if(upload==0):
-	os.system('git add .')
-	os.system('git commit -m "'+fileName+' add"')
-	os.system('git push origin master')
-	str='https://gaoqisen.github.io/GraphBed/'+fileName
-	txt(str)
+        adds='cd '+path+' ; git add .'
+        print(adds)
+        os.system(adds)
+        os.system('cd '+path+' ; git commit -m "'+fileName+' add"')
+        os.system('cd '+path+' ; git push origin master')
+        str='https://gaoqisen.github.io/GraphBed/'+fileName
+        txt(str)
